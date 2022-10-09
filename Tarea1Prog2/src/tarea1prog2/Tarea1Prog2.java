9f51a2b7f17d1bc11336cf08b406e21a9161de71
Mirko Peñailillo <Okrim017@gmail.com>
2022-10-08 21:56:20 -0300

    Definicion de main

--- Tarea1Prog2/src/tarea1prog2/Tarea1Prog2.java
+++ Tarea1Prog2/src/tarea1prog2/Tarea1Prog2.java
@@ -247,7 +247,88 @@ class OrdenCompra{
         
 public class Tarea1Prog2 {
     public static void main(String[] args) {
+        Articulo A1 = new Articulo((float)0.7,"pantalon","azul", 50000);
+        Articulo A2 = new Articulo((float)0.2,"polera","azul", 40000);
+        Articulo A3 = new Articulo((float)0.7,"poleron","azul", 60000);
+        Articulo A4 = new Articulo((float)0.8,"zapato","azul", 40000);
+        Articulo A5 = new Articulo((float)0.1,"gorro","azul", 15000);
         
+        Cliente C1 = new Cliente("Pedro","21.624.917-1");
+        Direccion D1 = new Direccion("Los Nardos 5089, Talcahuano, Bío Bío");
+        C1.setDireccion(D1);
+        Cliente C2 = new Cliente("Juan","20.953.216-8");
+        Direccion D2 = new Direccion("Volcán Copahue 574, Talcahuano, Bío Bío");
+        C2.setDireccion(D2);
+        
+        Date F1 = new Date();
+        
+        OrdenCompra OC1 = new OrdenCompra(F1,"Entregado");
+        DetalleOrden DO10 = new DetalleOrden(5);
+        DO10.setArticulo(A5);
+        DetalleOrden DO11 = new DetalleOrden(3);
+        DO11.setArticulo(A2);
+        DetalleOrden DO12 = new DetalleOrden(1);
+        DO12.setArticulo(A1);
+        OC1.add(DO10);
+        OC1.add(DO11);
+        OC1.add(DO12);
+        Factura DT1 = new Factura("45174",C1.getrut(),F1);
+        OC1.setDocTributario(DT1);
+        Efectivo P1 = new Efectivo(300000,F1);
+        
+        OrdenCompra OC2 = new OrdenCompra(F1,"Entregado");
+        DetalleOrden DO20 = new DetalleOrden(3);
+        DO20.setArticulo(A1);
+        DetalleOrden DO21 = new DetalleOrden(2);
+        DO21.setArticulo(A3);
+        OC2.add(DO20);
+        OC2.add(DO21);
+        Boleta DT2 = new Boleta("35134",C2.getrut(),F1);
+        OC2.setDocTributario(DT2);
+        Transferencia P2 = new Transferencia(12000,F1,"Estado","2109214");
+        Transferencia P3 = new Transferencia(12000,F1,"Estado","2109214");
+        
+        OrdenCompra OC3 = new OrdenCompra(F1,"Entregado");
+        DetalleOrden DO30 = new DetalleOrden(7);
+        DO30.setArticulo(A4);
+        OC3.add(DO30);
+        Boleta DT3 = new Boleta("21374",C2.getrut(),F1);
+        OC3.setDocTributario(DT3);
+        Tarjeta P4 = new Tarjeta(12000,F1,"Debito","2184");
+        
+        
+        
+        System.out.println("Orden de compra 1\n" + OC1 + "\n\n" + C1 + "\n\n" + DT1 + "\n");
+        for(int i=0;i<OC1.sizeDetalleOrdenAR();i=i+1){
+            System.out.println("Detalle " + (i+1) + "\n" + OC1.getDetalleOrden(i) + "\n");
+        }
+        System.out.println("Peso total : " + OC1.calcPeso() 
+                + "kg\nPrecio sin IVA : " + OC1.calcPrecioSinIVA() + "CLP\nIVA : " 
+                + OC1.calcIVA() + "CLP\nPrecio total : " + OC1.calcPrecio() 
+                + "CLP\nPago\n" + P1 + "CLP\nDevolucion : " 
+                + P1.calcDevolucion(OC1.calcPrecio(),P1.getMonto()));
+        
+        System.out.print("\n---------------------------------------------------------------------------------\n");
+        
+        System.out.println("\nOrden de compra 2\n" + OC2 + "\n\n" + C2 + "\n\n" + DT2 + "\n");
+        for(int i=0;i<OC2.sizeDetalleOrdenAR();i=i+1){
+            System.out.println("Detalle " + (i+1) + "\n" + OC2.getDetalleOrden(i) + "\n");
+        }
+        System.out.println("Peso total : " + OC2.calcPeso() 
+                + "kg\nPrecio sin IVA : " + OC2.calcPrecioSinIVA() + "CLP\nIVA : " 
+                + OC2.calcIVA() + "CLP\nPrecio total : " + OC2.calcPrecio() 
+                + "CLP\nPago 1\n" + P2 + "\nPago2\n" + P3);
+        
+        System.out.print("\n---------------------------------------------------------------------------------\n");
+        
+        System.out.println("\nOrden de compra 3\n" + OC3 + "\n\n" + C2 + "\n\n" + DT3 + "\n");
+        for(int i=0;i<OC3.sizeDetalleOrdenAR();i=i+1){
+            System.out.println("Detalle " + (i+1) + "\n" + OC3.getDetalleOrden(i) + "\n");
+        }
+        System.out.println("Peso total : " + OC3.calcPeso() 
+                + "kg\nPrecio sin IVA : " + OC3.calcPrecioSinIVA() + "CLP\nIVA : " 
+                + OC3.calcIVA() + "CLP\nPrecio total : " + OC3.calcPrecio() 
+                + "CLP\nPago 1\n" + P4);
     }
     
 }
